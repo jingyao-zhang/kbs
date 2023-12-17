6fc9ea51daff27c8b40d7fc78caa7ec81cbb546b
Jingyao <cloudyao95@gmail.com>
2023-12-17 03:31:31 +0000

    Update dependencies for attestation-service

--- auto-build.sh
+++ auto-build.sh
@@ -1,6 +1,6 @@
-# make clean
+make clean
 cargo update
 make passport-issuer-kbs
 sudo make install-issuer-kbs
-# make passport-resource-kbs POLICY_ENGINE=opa
-# sudo make install-resource-kbs
+make passport-resource-kbs POLICY_ENGINE=opa
+sudo make install-resource-kbs
